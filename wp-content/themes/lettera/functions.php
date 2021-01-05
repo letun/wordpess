774d4d31b07e8030a4696163059ca0b6abd25fe2
letun <letun1984@gmail.com>
2021-01-05 21:41:06 +0400

    Move lettera post type from plugin to theme

--- wp-content/themes/lettera/functions.php
+++ wp-content/themes/lettera/functions.php
@@ -2,3 +2,5 @@
 
 require_once get_template_directory() . '/inc/disable_comments.php';
 require_once get_template_directory() . '/inc/disable_xmlrpc.php';
+
+require_once get_template_directory() . '/inc/lettera_post_type.php';
---
+++ wp-content/themes/lettera/inc/lettera_post_type.php
@@ -0,0 +1,36 @@
+<?php
+
+add_action('init', function() {
+	$labels = array(
+		'name' => _x('Lettera', 'post type general name'),
+		'singular_name' => _x('Lettera Email', 'post type singular name'),
+		'add_new' => _x('Add New', 'guide'),
+		'add_new_item' => __('Add new email'),
+		'edit_item' => __('Edit email'),
+		'new_item' => __('New email'),
+		'view_item' => __('View email'),
+		'search_items' => __('Search emails'),
+		'not_found' =>  __('Nothing found'),
+		'not_found_in_trash' => __('Nothing found in Trash'),
+		'parent_item_colon' => ''
+	);
+
+	$args = array(
+		'labels' => $labels,
+		'menu_icon' => 'dashicons-buddicons-pm',
+		'public' => true,
+		'publicly_queryable' => true,
+		'show_ui' => true,
+		'query_var' => true,
+		'capability_type' => 'post',
+		'hierarchical' => false,
+		'menu_position' => null,
+		'supports' => array('title', 'editor', 'author', 'custom-fields'),
+		'rewrite' => array('slug' => 'lettera', 'with_front' => true),
+		'taxonomies' => array('category', 'post_tag'),
+		'show_in_rest' => true,
+	);
+
+	register_post_type('lettera' , $args);
+	//add_rewrite_rule('^lettera', 'index.php?post_type=lettera', 'top');
+});
