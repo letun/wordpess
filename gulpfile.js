78b04574c223e99f22a5d8da0a66b828feb5fe35
letun <letun1984@gmail.com>
2021-01-09 22:50:03 +0400

    Add php/js/css lint files

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,10 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var jslint = require('gulp-jslint');
+var phplint = require('gulp-phplint');
+var scsslint = require('gulp-stylelint');
+var postcss = require('gulp-postcss');
 
 sass.compiler = require('node-sass');
 
@@ -11,6 +15,57 @@ gulp.task('sass', function () {
 		.pipe(gulp.dest('./wp-content/themes/lettera/'));
 });
 
+var addMargin = function (css, opts) {
+	css.walkDecls(function(decl) {
+		if (decl.prop === 'margin') {
+			decl.parent.insertAfter(decl, {
+				prop: 'Margin',
+				value: decl.value
+			});
+		}
+	});
+};
+
+gulp.task('addMargin', function () {
+	var plugins = [
+		addMargin
+	];
+	return gulp.src('./wp-content/themes/lettera/lettera.css')
+		.pipe(postcss(plugins))
+		.pipe(gulp.dest('./wp-content/themes/lettera/lettera.css'));
+});
+
 gulp.task('watch', function () {
 	gulp.watch('./wp-content/themes/lettera/**/*.scss', gulp.series('sass'));
 });
+
+gulp.task('phplint', function () {
+	return gulp.src(['./wp-content/themes/lettera/**/*.php'])
+		.pipe(phplint('', {
+			skipPassedFiles: true
+		}))
+		.pipe(phplint.reporter(function(file){
+			var report = file.phplintReport || {};
+			if (report.error) {
+				console.error(report.message+' on line '+report.line+' of '+report.filename);
+			}
+		}));
+});
+
+gulp.task('jslint', function () {
+	return gulp.src('./wp-content/themes/lettera/**/*.js')
+		.pipe(jslint({ /* this object represents the JSLint directives being passed down */ }))
+		.pipe(jslint.reporter('default', true));
+});
+
+gulp.task('scsslint', function lintCssTask() {
+	return gulp
+		.src('./wp-content/themes/lettera/scss/lettera/_alignment.scss')
+		.pipe(scsslint({
+			reporters: [
+				{formatter: 'string', console: true}
+			]
+		}));
+});
+
+gulp.task("test", gulp.series("phplint", "jslint") );
--- wp-content/themes/lettera/js/func.js
+++ wp-content/themes/lettera/js/func.js
@@ -1,73 +1,61 @@
+'use strict';
+
 (function() {
-	var frame = document.querySelector('.content__frame');
-	var ruler = document.querySelector('.ruler');
-	var blacklist_rulers = ['chevron', 'selectboxes', 'radiobuttons', 'checkboxes', 'inputs', 'buttons', 'headers'];
-	var breakpoints = {
-		auto: '160', //auto
-		xxs: '320',
-		xs: '414',
-		s: '480',
-		m: '768',
-		l: '1024',
-		xl: '1100',
-		xxl: '1200',
-		xxxl: '1380',
-		xxxxl: '1440'
-	}
+  var frame = document.querySelector('.content__frame');
+  var ruler = document.querySelector('.ruler');
+  var blacklist_rulers = ['chevron', 'selectboxes', 'radiobuttons', 'checkboxes', 'inputs', 'buttons', 'headers'];
+  var breakpoints = {
+    auto: '160', //auto
+    xxs: '320',
+    xs: '414',
+    s: '480',
+    m: '768',
+    l: '1024',
+    xl: '1100',
+    xxl: '1200',
+    xxxl: '1380',
+    xxxxl: '1440'
+  };
 
-	function openPage(item) {
-		var page = item.getAttribute('href').substring('1');
-		frame.classList.add('content__frame--loading');
-		frame.setAttribute('src', 'includes/' + page + '.php');
+  function setFrameWidth(w) {
+    frame.style.width = w >= 320 ? w + 'px' : null;
+  }
 
-		var item_levels = item.hash.substr(1).split('.');
-		document.getElementsByClassName("content__frame")[0].removeAttribute("style");
-		if ((blacklist_rulers.indexOf(item_levels[1]) !== -1) || (typeof(item_levels[1]) == 'undefined') ) {
-			document.getElementsByClassName("ruler")[0].style.display = "none";
-			if (item_levels[0] == 'calypso') {
-				document.getElementsByClassName("content__frame")[0].setAttribute("style", "width: 100%; margin: 0;")
-			}
-		} else {
-			document.getElementsByClassName("ruler")[0].removeAttribute("style");
-		}
-	}
+  function initRuler() {
+    var div;
+    var i;
+    var lastWidth = -20;
 
-	function setFrameWidth(w) {
-		frame.style.width = w >= 320 ? w + 'px' : null;
-	}
+    for (i in breakpoints) {
+      div = document.createElement('div');
+      div.innerHTML = (breakpoints[i] >= 320 ? breakpoints[i] : 'Auto');
+      div.className = 'ruler__button';
+      div.style.width = parseInt(breakpoints[i]) - lastWidth + 'px';
+      div.addEventListener('click', (function(w) {return function() {
+        setFrameWidth(w);
+        this.parentNode.querySelector('.ruler__button--active').classList.remove('ruler__button--active');
+        this.classList.add('ruler__button--active');
+      }})(breakpoints[i]));
+      ruler.appendChild(div);
 
-	function initRuler() {
-		var i, len, div, lastWidth = -20;
-		for (i in breakpoints) {
-			div = document.createElement('div');
-			div.innerHTML = (breakpoints[i] >= 320 ? breakpoints[i] : 'Auto');
-			div.className = 'ruler__button';
-			div.style.width = parseInt(breakpoints[i]) - lastWidth + 'px';
-			div.addEventListener('click', (function(w) {return function() {
-				setFrameWidth(w);
-				this.parentNode.querySelector('.ruler__button--active').classList.remove('ruler__button--active');
-				this.classList.add('ruler__button--active');
-			}})(breakpoints[i]));
-			ruler.appendChild(div);
+      lastWidth = parseInt(breakpoints[i]);
+    }
+    ruler.firstChild.classList.add('ruler__button--active');
+  }
 
-			lastWidth = parseInt(breakpoints[i]);
-		}
-		ruler.firstChild.classList.add('ruler__button--active');
-	}
+  function init() {
+    initRuler();
 
-	function init() {
-		initRuler();
+    window.addEventListener('message', function(e) {
+      try {
+        var data = JSON.parse(e.data);
+        if (data.method === 'frameHeight') {
+          frame.style.height = data.data + 'px';
+        }
+      }
+      catch (e) {}
+    });
 
-		window.addEventListener('message', function(e) {
-			try {
-				var data = JSON.parse(e.data);
-				if (data.method == 'frameHeight') {
-					frame.style.height = data.data + 'px';
-				}
-			}
-			catch (e) {}
-		});
-
-	}
-	init();
+  }
+  init();
 })();
