abadf8ab409518f43018119d81294436c423172e
letun <letun1984@gmail.com>
2021-03-08 18:34:13 +0400

    Update plugin Cloudflare (4.1.0 -> 4.2.1)

--- wp-content/plugins/cloudflare/vendor/autoload.php
+++ wp-content/plugins/cloudflare/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInitc18900f6a542da4a0b530114484e7cc6::getLoader();
+return ComposerAutoloaderInit7545ca8cb46c9c484c9a7ce09ac73f4c::getLoader();
